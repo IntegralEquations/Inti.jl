8605b383b53f1002afef8d8981c0da243c56f655
Documenter.jl <documenter@juliadocs.github.io>
2023-09-29 19:23:42 +0000

    build based on 0fc5318

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Inti","category":"page"},{"location":"#Inti","page":"Home","title":"Inti","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Inti.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Inti]","category":"page"}]
+}
