3b8bfb10bf216fe42345344d99db94be8c34ba04
Luiz M. Faria <maltezfaria@gmail.com>
2023-10-02 16:06:08 +0200

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,8 @@ using Aqua
 
 @safetestset "Code quality" include("aqua_test.jl")
 
+@safetestset "Utility functions" include("utils_test.jl")
+
 @safetestset "Reference shapes" include("reference_shapes_test.jl")
 
 @safetestset "Reference interpolation" include("reference_interpolation_test.jl")
