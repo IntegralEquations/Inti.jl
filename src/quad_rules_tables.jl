88eaf08c7b2ce9c4f4fa049f3a3d8ebec01c3dee
Thomas G. Anderson <tanderson@alumni.caltech.edu>
2023-10-08 22:05:08 -0500

    Add Vioreanu-Rokhlin interpolatory quadratures on triangles

--- src/quad_rules_tables.jl
+++ src/quad_rules_tables.jl
@@ -2,6 +2,9 @@
 Tabulated Gaussian quadrature rules from GMSH.
 Obtained from `https://gitlab.onelab.info/gmsh/gmsh/-/blob/gmsh_4_7_0/Numeric/GaussQuadratureTri.cpp`
 and `https://gitlab.onelab.info/gmsh/gmsh/-/blob/gmsh_4_7_0/Numeric/GaussQuadratureTet.cpp`.
+
+Also includes Vioreanu-Rokhlin interpolating quadratures, see
+`Spectra of Multiplication Operators as a Numerical Tool', B. Vioreanu and V. Rokhlin, SIAM J Sci. Comput. (2014).
 =#
 
 include("quad_rules_tables_tetrahedron.jl")
@@ -38,6 +41,30 @@ const TRIANGLE_GAUSS_ORDER_TO_NPTS = Dict(1 => 1,
                                           12 => 33)
 const TRIANGLE_GAUSS_NPTS_TO_ORDER = Dict((v, k) for (k, v) in TRIANGLE_GAUSS_ORDER_TO_NPTS)
 
+const TRIANGLE_VR_QRULES = Dict(1 => TRIANGLE_VR1N1,
+                                3 => TRIANGLE_VR2N3,
+                                6 => TRIANGLE_VR4N6,
+                                10 => TRIANGLE_VR5N10,
+                                15 => TRIANGLE_VR7N15,
+                                21 => TRIANGLE_VR9N21,
+                                28 => TRIANGLE_VR11N28,
+                                36 => TRIANGLE_VR12N36,
+                                45 => TRIANGLE_VR14N45,
+                                55 => TRIANGLE_VR16N55,
+                                66 => TRIANGLE_VR17N66)
+const TRIANGLE_VR_ORDER_TO_NPTS = Dict(1 => 1,
+                                       2 => 3,
+                                       4 => 6,
+                                       5 => 10,
+                                       7 => 15,
+                                       9 => 21,
+                                       11 => 28,
+                                       12 => 36,
+                                       14 => 45,
+                                       16 => 55,
+                                       17 => 66)
+const TRIANGLE_VR_NPTS_TO_ORDER = Dict((v, k) for (k, v) in TRIANGLE_VR_ORDER_TO_NPTS)
+
 const TETAHEDRON_GAUSS_QRULES = Dict(1 => TETAHEDRON_G1N1,
                                      4 => TETAHEDRON_G2N4,
                                      5 => TETAHEDRON_G3N5,
@@ -53,6 +80,14 @@ const TETRAHEDRON_GAUSS_ORDER_TO_NPTS = Dict(1 => 1, 2 => 4, 3 => 5, 4 => 11, 5 
 const TETRAHEDRON_GAUSS_NPTS_TO_ORDER = Dict((v, k)
                                              for (k, v) in TETRAHEDRON_GAUSS_ORDER_TO_NPTS)
 
+# TODO
+const TETRAHEDRON_VR_QRULES = Dict()
+const TETRAHEDRON_VR_ORDER_TO_NPTS = Dict()
+const TETRAHEDRON_VR_NPTS_TO_ORDER = Dict((v, k) for (k, v) in TETRAHEDRON_VR_ORDER_TO_NPTS)
+
 ##
 const GAUSS_QRULES = Dict(ReferenceTriangle => TRIANGLE_GAUSS_QRULES,
                           ReferenceTetrahedron => TETAHEDRON_GAUSS_QRULES)
+
+const VR_QRULES = Dict(ReferenceTriangle => TRIANGLE_VR_QRULES,
+                       ReferenceTetrahedron => TETRAHEDRON_VR_QRULES)
