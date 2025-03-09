008ee887b095f737093223d475663027dd7e1365
Dongchen He <dongchen.he@polytechnique.edu>
2025-03-10 00:11:14 +0800

    make discontinuous integral test work for Stokes

--- thesis_tests/test_files/Integral_discontinuous.jl
+++ thesis_tests/test_files/Integral_discontinuous.jl
@@ -3,7 +3,8 @@
 # u = x -> 0 ≤ x % (2π) < π || -2π ≤ x % (2π) < -π ? 1 : 0
 # bu = x -> quadgk(y -> b(y) * u(x - y), -ε, ε, atol=1e-15)[1]
 # bu = x -> sin(x)
-u = x -> x[2] > 0 ? 1 : 0
+# u = x -> x[2] > 0 ? 1 : 0
+u = x -> x[2] > 0 ? SVector(0, 1) : SVector(0, -1)
 # u = x -> x[2]
 # u = x -> x[2] / norm(x)
 # u = x -> 1
@@ -29,7 +30,6 @@ for qorder in Q
         xs = qs.coords
         @info xs
 
-
         uvec = map(u, Inti.coords.(quad))
         # uvec = [bu(atan(x[2], x[1])) for x in Inti.coords.(quad)]
         uvec_norm = norm(uvec, Inf)
@@ -45,8 +45,8 @@ for qorder in Q
             G(xs, χ(s)) * u(χ(s)) * norm(ForwardDiff.derivative(χ, s))        
         end
         @show err_ref
-        
-        e0 = norm(Smat ⋅ uvec - ref, Inf) / uvec_norm
+
+        e0 = norm((Smat * uvec)[1] - ref, Inf) / uvec_norm
 
         green_multiplier = fill(-0.5, length(quad))
         # δS, δD = Inti.bdim_correction(pde, quad, quad, Smat, Dmat; green_multiplier)
@@ -76,13 +76,13 @@ for qorder in Q
         #     compression = (method = :none,),
         #     correction  = (method = :ldim,),
         # )
-        eloc = norm(Sdim ⋅ uvec - ref, Inf) / uvec_norm
+        eloc = norm((Sdim * uvec)[1] - ref, Inf) / uvec_norm
 
         tdim = @elapsed δS, δD =
             Inti.bdim_correction(pde, [xs], quad, Smat, Dmat; green_multiplier)
         Sdim = Smat + δS
         Ddim = Dmat + δD
-        eglo = norm(Sdim ⋅ uvec - ref, Inf) / uvec_norm
+        eglo = norm((Sdim * uvec)[1] - ref, Inf) / uvec_norm
         # @show norm(e0, Inf)
         @show eloc
         @show eglo
